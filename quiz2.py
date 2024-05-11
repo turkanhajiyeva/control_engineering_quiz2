f663ca435e088352ae9c5ae0c4574616619d14d4
turkanhajiyeva <thajiyeva15574@ada.edu.az>
2024-05-11 20:07:22 +0400

    lost my mind

---
+++ quiz2.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import control.matlab as cn
+
+t = np.arange(0, 15, 0.01)
+
+G = cn.ss([[0, 1, 0], [0, 0, 1], [1, -5, -3]], [[0], [0], [1]], [1, 0, 0], [0])
+
+cn.pzmap(G)
+plt.figure()
+A = np.array([[0, 1, 0], [0, 0, 1], [1, -5, -3]])
+K = np.array([[34, 14, 6]])
+B = np.array([[0], [0], [1]])
+
+Anew = A - B@K
+Gnew = cn.ss(Anew, [[0], [0], [1]], [1, 0, 0], [0])
+
+cn.pzmap(Gnew)
+
+print(cn.zpk2tf([], [-1+2j, -1-2j, -7], 1))
+
+y, T = cn.step(Gnew, t)
+plt.plot(t, y)
+plt.figure()
+y, T = cn.step(G, t)
+plt.plot(t, y)
+
+plt.show()
