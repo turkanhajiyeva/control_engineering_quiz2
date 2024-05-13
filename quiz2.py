175ad4c6f7d86542326607c1b542da03b56379e6
turkanhajiyeva <thajiyeva15574@ada.edu.az>
2024-05-13 18:53:45 +0400

    done

--- quiz2.py
+++ quiz2.py
@@ -7,6 +7,7 @@ t = np.arange(0, 15, 0.01)
 G = cn.ss([[0, 1, 0], [0, 0, 1], [1, -5, -3]], [[0], [0], [1]], [1, 0, 0], [0])
 
 cn.pzmap(G)
+plt.title("Pole-Zero Map of G")
 plt.figure()
 A = np.array([[0, 1, 0], [0, 0, 1], [1, -5, -3]])
 K = np.array([[34, 14, 6]])
@@ -16,13 +17,16 @@ Anew = A - B@K
 Gnew = cn.ss(Anew, [[0], [0], [1]], [1, 0, 0], [0])
 
 cn.pzmap(Gnew)
-
+plt.title("Pole-Zero Map of Gnew")
+plt.figure()
 print(cn.zpk2tf([], [-1+2j, -1-2j, -7], 1))
 
 y, T = cn.step(Gnew, t)
 plt.plot(t, y)
+plt.title("Step Response of Gnew")
 plt.figure()
 y, T = cn.step(G, t)
 plt.plot(t, y)
+plt.title("Step Response of G")
 
 plt.show()
